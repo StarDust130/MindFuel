5dca95fa7f5132242abe007f93f006761142826f
ChandraShekhar <csyadav0513@gmail.com>
2024-10-09 10:55:08 +0530

    Refactor package dependencies and add form component

--- client/src/app/(auth)/layout.tsx
+++ client/src/app/(auth)/layout.tsx
@@ -1,13 +1,7 @@
-import AuthProvider from "@/context/auth-context";
-
 export default function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <main>
-      <AuthProvider>{children}</AuthProvider>
-    </main>
-  );
+  return <main>{children}</main>;
 }
