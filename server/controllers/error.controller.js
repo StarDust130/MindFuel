c376eaf079404c95e8206cda018fa832ea5bdc37
ChandraShekhar <csyadav0513@gmail.com>
2024-10-16 11:49:42 +0530

    Refactor globalErrorHandler in error.controller.js to include stack trace in development mode

--- server/controllers/error.controller.js
+++ server/controllers/error.controller.js
@@ -6,6 +6,14 @@ export const globalErrorHandler = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error 😿";
 
+  if (process.env.NODE_ENV === "development") {
+    return res.status(err.statusCode).json({
+      success: false,
+      message: err.message,
+      stack: err.stack,
+    });
+  }
+
   res.status(err.statusCode).json({
     success: false,
     message: err.message,
