aa787cf7b923addf0f915360448ef85876d79c89
ChandraShekhar <csyadav0513@gmail.com>
2024-10-17 20:56:26 +0530

    Refactor JWT token expiration to 7 days

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -84,39 +84,37 @@ export const loginUser = catchAsync(async (req, res) => {
     });
   }
 
-  // 4) Create JWT token
-  const token = jwt.sign(
-    {
-      _id: user._id,
-      username: user.username,
-      email: user.email,
-      role: user.role,
-    },
-    JWT_SECRET,
-    { expiresIn: "2h" }
-  );
+  // 4) Create JWT token with 5-second expiration
+ const token = jwt.sign(
+   {
+     _id: user._id,
+     username: user.username,
+     email: user.email,
+     role: user.role,
+   },
+   JWT_SECRET,
+   { expiresIn: "7d" } // Token expires in 7 days
+ );
 
-  // 5) Set JWT as HttpOnly cookie
  res.cookie("accessToken", token, {
-   httpOnly: false, // Set to false to allow access via JavaScript
-   secure: process.env.NODE_ENV === "production", // Only use secure in production
-   sameSite: "Lax", // Or adjust based on your cross-site needs
-   maxAge: 3600 * 1000, // 1 hour expiration
-   path: "/", // Make the cookie accessible on all routes
+   httpOnly: true,
+   secure: process.env.NODE_ENV === "production",
+   sameSite: "Lax",
+   maxAge: 7 * 24 * 60 * 60 * 1000, // Cookie expires in 7 days (7 days * 24 hours * 60 minutes * 60 seconds * 1000 milliseconds)
+   path: "/",
  });
 
-
- 
 
   // 6) Return success response
   return res.status(200).json({
     success: true,
     message: "Logged in successfully.",
     data: {
-      accesToken: token,
+      accessToken: token,
     },
   });
 });
+
 
 //! Logout user 🗒️
 export const logoutUser = catchAsync(async (req, res) => {
