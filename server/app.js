d1749ef9e1b4b02923dea9f0d1c8e5a0c3d7a11b
ChandraShekhar <csyadav0513@gmail.com>
2024-10-20 16:12:32 +0530

    Refactor rate limiting middleware in app.js to increase the time window to 1 hour and limit each IP to 500 requests per hour

--- server/app.js
+++ server/app.js
@@ -20,8 +20,8 @@ app.use(
 
 //! Rate Limiting Middleware
 const limiter = rateLimit({
-  windowMs: 60 * 60 * 1000, // 15 minutes
-  max: 1000, // limit each IP to 100 requests per windowMs
+  windowMs: 60 * 60 * 1000, // 1 hour window
+  max: 500, // limit each IP to 500 requests per windowMs
   message: "Too many requests from this IP, please try again after an hour",
 });
 
