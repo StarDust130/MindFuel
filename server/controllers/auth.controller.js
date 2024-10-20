6564f84a534fb39e29efe3a004721df74c4ccd82
ChandraShekhar <csyadav0513@gmail.com>
2024-10-20 13:48:04 +0530

    Refactor resetPassword function in auth controller to handle password reset based on token

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -3,6 +3,7 @@ import jwt from "jsonwebtoken";
 import { catchAsync } from "../utils/catchAsync.js";
 import { AppError } from "../utils/appError.js";
 import sendEmail from "../utils/email.js";
+import crypto from "crypto";
 
 // Load environment variables
 const JWT_SECRET = process.env.JWT_SECRET || "default_secret";
@@ -246,4 +247,34 @@ export const forgotPassword = catchAsync(async (req, res, next) => {
 });
 
 //! Reset password 🔑
-export const resetPassword = catchAsync(async (req, res, next) => {});
+export const resetPassword = catchAsync(async (req, res, next) => {
+  // 1) Get user based on the token
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(req.params.token)
+    .digest("hex");
+
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() },
+  });
+
+  // 2) If token has not expired, and there is user, set the new password
+
+  if (!user) {
+    return next(new AppError("Token is invalid or has expired.", 400));
+  }
+
+  // 3) Update changedPasswordAt property for the user
+  user.password = req.body.password;
+
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+
+  await user.save();
+
+  // 4) Log the user in, send JWT
+
+
+  
+});
