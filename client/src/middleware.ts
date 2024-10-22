08f0a4a6411af60d08448c93818928f3359ea2aa
ChandraShekhar <csyadav0513@gmail.com>
2024-10-22 18:13:33 +0530

    Refactor middleware to use accessToken instead of refreshToken for authentication

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -2,18 +2,18 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-  const refreshToken = req.cookies.get("refreshToken")?.value;
+  const accessToken = req.cookies.get("accessToken")?.value;
 
   const pathname = req.nextUrl.pathname;
 
   // 1. User is not authenticated, redirect to login unless they are already on login or sign-up
-  if (!refreshToken && pathname !== "/login" && pathname !== "/sign-up") {
+  if (!accessToken && pathname !== "/login" && pathname !== "/sign-up") {
     const loginUrl = new URL("/login", req.url);
     return NextResponse.redirect(loginUrl);
   }
 
   // 2. User is authenticated, prevent access to login or sign-up
-  if (refreshToken && (pathname === "/login" || pathname === "/sign-up")) {
+  if (accessToken && (pathname === "/login" || pathname === "/sign-up")) {
     const profileUrl = new URL("/profile", req.url);
     return NextResponse.redirect(profileUrl);
   }
