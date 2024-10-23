87d8f03036740d19221029bea0b99f45300435c5
ChandraShekhar <csyadav0513@gmail.com>
2024-10-23 12:15:28 +0530

    Refactor user routes to add deleteMe and updateMe functions

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -6,8 +6,10 @@ import {
 } from "../middlewares/auth.middlewares.js";
 import {
   deleteAll,
+  deleteMe,
   getAllInfo,
   getAllUsers,
+  updateMe,
 } from "../controllers/user.controller.js";
 
 const router = express();
@@ -16,12 +18,24 @@ const router = express();
 //!  Admin Routes
 router.get("/", protectRoute, restrictRoute(Roles.ADMIN), getAllUsers);
 
+//! delete All users
 router.delete(
   "/deleteAll",
   protectRoute,
   restrictRoute(Roles.ADMIN),
   deleteAll
 );
+
+//! update  user
+router.patch("/updateMe", protectRoute, restrictRoute(Roles.ADMIN), updateMe);
+
+
+//! delete user
+router.delete("/deleteMe", protectRoute, restrictRoute(Roles.ADMIN), deleteMe);
+
+
+
+//! getAllInfo
 router.get("/getAllInfo", protectRoute, restrictRoute(Roles.ADMIN), getAllInfo);
 
 export default router;
