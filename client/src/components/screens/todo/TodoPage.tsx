53ac50f70ef8fe6a94a1f9d10a0c12645f2e9ad8
ChandraShekhar <csyadav0513@gmail.com>
2024-10-25 11:36:35 +0530

    Add TodoCard component and related files

--- client/src/components/screens/todo/TodoPage.tsx
+++ client/src/components/screens/todo/TodoPage.tsx
@@ -1,7 +1,28 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import TodoTable from "./TodoTable"
+
 
 const TodoPage = () => {
   return (
-    <div>TodoPage</div>
+    <>
+
+    <div className="flex flex-col py-20 items-center h-screen w-full px-10">
+
+        <h1 className="text-center text-4xl font-bold">Todo</h1>
+
+        <div className="flex justify-center items-center w-1/2 gap-2 mt-3">
+            <Input placeholder="Add Todo" />
+            <Button variant={"default"}>Add Todo</Button>
+        </div>
+        <TodoTable />
+
+        
+
+
+    </div>
+    
+    </>
   )
 }
 export default TodoPage
---
+++ client/src/components/screens/todo/TodoTable.tsx
@@ -0,0 +1,10 @@
+import TodoCard from "./TodoCard";
+
+const TodoTable = () => {
+  return (
+    <div className="w-full mt-10 flex justify-center items-center border-gray-500 border-2 border-dotted">
+      <TodoCard />
+    </div>
+  );
+}
+export default TodoTable
